e8b5062e1cf3f16d1435442ef6e8f1934315e7d4
Dimitris Karnikis <dimkar407@gmail.com>
2021-05-04 18:56:39 +0300

    Create .c

---
+++ .c
@@ -0,0 +1,1 @@
+curl   -X POST   -H "Accept: application/vnd.github.v3+json"   -H "Authorization: Bearer $GH_TOKEN"   https://api.github.com/repos/dkarnikis/circle-ci-bot/pull/3/comments   -d '{"body":"Hello Github!"}'
