df34ceec896672f4a7aeffc03be348ba146b76f4
DIMITRIS KARNIKIS <dkarnikis@gmail.com>
2021-05-04 21:59:56 +0300

    xd

--- .c
+++ .c
@@ -1,3 +1,4 @@
- curl -s -H "Authorization: token ${GH_TOKEN}" \
- -X POST -d '{"body": $(tail -n 10 results.log)}' \
+v=$(tail -n 10 /pash/results.log) 
+curl -s -H "Authorization: token ${GH_TOKEN}" \
+ -X POST -d '{"body": $v }' \
  "https://api.github.com/repos/dkarnikis/circle-ci-bot/issues/3/comments"
