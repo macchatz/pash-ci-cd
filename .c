251baac8eef843cfd1defba73374e48d75bbe16d
DIMITRIS KARNIKIS <dkarnikis@gmail.com>
2021-05-04 19:22:26 +0300

    xd

--- .c
+++ .c
@@ -1,2 +1,4 @@
 apt-get update; apt-get install curl
 curl   -X POST   -H "Accept: application/vnd.github.v3+json"   -H "Authorization: Bearer $GH_TOKEN"   https://api.github.com/repos/dkarnikis/circle-ci-bot/pull/3/comments   -d '{"body":"Hello Github!"}'
+
+
