59f8d9d283e00c4a6265348ff47812c8f8adcc78
DIMITRIS KARNIKIS <dkarnikis@gmail.com>
2021-05-04 23:56:29 +0300

    bot comment

--- .c
+++ .c
@@ -1,5 +1,4 @@
 date=$(LANG=en_us_88591; date)
-apt-get install jq
 jq -n --arg msg "$(echo ${date}; echo ''; tail -n 10 /pash/results.log )" '{body: $msg}' > git-tmp.txt
 curl -s -H "Authorization: token ${GH_TOKEN}" \
          -X POST -d @git-tmp.txt  \
