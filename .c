d5cc1f4f9eb19e4cec4f83abef1e2922c73e602e
DIMITRIS KARNIKIS <dkarnikis@gmail.com>
2021-05-07 10:55:43 +0300

    bot comment

--- .c
+++ .c
@@ -1,6 +1,8 @@
+# get the timer in english
 date=$(LANG=en_us_88591; date)
-echo $1
-jq -n --arg msg "$(echo ${date}; echo ''; tail -n 10 /pash/results.log )" '{body: $msg}' > git-tmp.txt
+# generate the json for the gh message
+jq -n --arg msg "$(echo ${date}; echo ''; cat /pash/results.log )" '{body: $msg}' > git-tmp.txt
+# post the message to the gh
 curl -s -H "Authorization: token ${1}" \
          -X POST -d @git-tmp.txt  \
          "https://api.github.com/repos/dkarnikis/circle-ci-bot/issues/3/comments"
