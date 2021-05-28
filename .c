71cddd72eb4c6659cb6d7f58797066354b348e3e
DIMITRIS KARNIKIS <dkarnikis@gmail.com>
2021-05-28 17:26:54 +0300

    Fixes

--- .c
+++ .c
@@ -1,7 +1,7 @@
 # get the timer in english
 date=$(LANG=en_us_88591; date)
 # generate the json for the gh message
-jq -n --arg msg "$(echo ${date}; echo ''; cat /pash/results.log )" '{body: $msg}' > git-tmp.txt
+jq -n --arg msg "$(echo ${date}; echo ''; cat /pash/scripts/results.log )" '{body: $msg}' > git-tmp.txt
 # post the message to the gh
 curl -s -H "Authorization: token ${1}" \
          -X POST -d @git-tmp.txt  \
